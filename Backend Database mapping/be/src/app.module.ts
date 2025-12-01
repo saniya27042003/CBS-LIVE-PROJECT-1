db58f9ee509054f0dd7412dea680ef43f748e835
saniya27042003 <saniyar738@gmail.com>
2025-12-01 12:08:42 +0530

    Use env-based primary DB config

--- Backend Database mapping/be/src/app.module.ts
+++ Backend Database mapping/be/src/app.module.ts
@@ -1,16 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 import { DatabaseModule } from './database/database.module';
 import { DatabaseMappingModule } from './database-mapping/database-mapping.module';
 import { primaryDBConfig } from './orm.config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot(primaryDBConfig),
-    // TypeOrmModule.forRoot(clientDBConfig),
     DatabaseModule,
     DatabaseMappingModule,
-    // clientDBConfig
   ],
 })
 export class AppModule { }
