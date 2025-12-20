3cac18545deebc012f93098f3a4ecf29e74b5a86
pranavd33 <pranavdeshpande331@gmail.com>
2025-12-20 15:23:58 +0530

    Final

--- Backend Database mapping/be/src/app.module.ts
+++ Backend Database mapping/be/src/app.module.ts
@@ -9,27 +9,8 @@ import { DatabaseMappingModule } from './database-mapping/database-mapping.modul
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule,AuthModule],
-      inject: [ConfigService],
-      useFactory: async (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get<string>('DB_HOST'),
-        port: Number(config.get<string>('DB_PORT')),
-        username: config.get<string>('DB_USER'),
-        password: config.get<string>('DB_PASS'),
-        database: config.get<string>('DB_NAME'),
-        // add all other entities you need here
-        synchronize: false,
-        logging: false,
-      }),
-    }),
-
-    // Feature modules
     DatabaseMappingModule,
-  
     AuthModule,
   ],
 })
-export class AppModule { }
+export class AppModule {}
